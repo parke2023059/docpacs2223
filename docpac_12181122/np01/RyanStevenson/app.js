5d38e7fa326a43a9965a293f26b7578afd7e1daf
Ryan24313 <79584392+Ryan24313@users.noreply.github.com>
2022-11-15 14:13:16 -0500

    Docpac 12

---
+++ docpac_12181122/np01/RyanStevenson/app.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+var parsedData = '';
+
+// This is how you read files synchronously
+var data = fs.readFileSync('users.json');
+// Convert buffer to Js object
+var parsedData = JSON.parse(data);
+
+app.get('/', (request, response) => {
+	response.render('users', { users: parsedData.users })
+})
+
+app.get('/update', (request, response) => {
+	if (request.query.name) {
+		console.log({ name: request.query.name, id: parsedData.users.length - 1 });
+		parsedData.users.push({ name: request.query.name, id: parsedData.users.length })
+		let json = JSON.stringify(parsedData)
+		fs.writeFileSync('users.json', json)
+		response.redirect('/')
+	} else response.send('you need to add a name')
+})
+
+
+app.listen(3000);

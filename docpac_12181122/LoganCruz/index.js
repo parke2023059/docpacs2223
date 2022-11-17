2a65758676c49de8a9f5b8c3c176e5ed0c59c35e
Logan-cruz <logan24378@live.ytech.edu>
2022-11-17 14:58:25 -0500

    Project

--- docpac_12181122/LoganCruz/index.js
+++ docpac_12181122/LoganCruz/index.js
@@ -1,44 +1,34 @@
-const fs = require('fs')
-const json = require('./users.json')
-const ejs = require('ejs')
-const express = require('express')
-const http = require ('http')
-const port = 3000
-const app = express()
-var parsedData
- // this is how you read files asynchromonously
- fs.readFile('users.json', function (err, data) {
-   // Validate the data
-   if (err){
-    console.log(err);
-   }else{
-    //convert raw bytes into JS object with JSON.parse();
-     parsedData = JSON.parse(data)
-     console.log(parsedData);
-}
-});
-app.set('view engine', 'ejs')
-// GET method route
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const fs = require('fs');
+app.use( bodyParser.urlencoded( {extended: true} ) );
+app.set('view engine', 'ejs');
+var customerData = JSON.parse(fs.readFileSync('customers.json'));
 app.get('/', (req, res) => {
-  res.render('index'), {
-    users : parsedData
+    res.render('index', { ejsData: customerData});
+});
+app.post('/createorder', (req, res)=>{
+  let newOrder = {
+    id: customerData.lastOrder++,
+    name:req.body.customerName,
+    item:[]
   }
+  customerData.orders.push(newOrder)
+  fs.writeFileSync('customer.json', JSON.stringify(customerData))
+  console.log(req.body)
+  res.send('It worked boss.')
 })
+app.get('/vieworder', (req,res) =>{
 
-app.listen(port, () => {
-  console.log(`server listening at port ${port}`)
+  console.log(req.query.id)
+  let thisOrder = customerData.orders.find((order)=> order.id == req.query.id)
+  console.log(thisOrder)
+  res.render('vieworder')
 })
-
-//this is how you read files synchromously
-/*
-var data = fs.readFileSync('users.json')
-//convert buffer to js dara
-parsedData = JSON.parse(data)
-
-
-console.log(parsedData);
-
-console.log(parsedData.users[0].name)
-*/
-
-//html = ejs.render('<% ')
+app.get('/neworder', (req,res)=>{
+  res.render('neworder')
+})
+app.listen(3000,
+    () => { console.log('server started')}
+);

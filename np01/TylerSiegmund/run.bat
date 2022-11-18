b7039c3bafd3d8ad38c6c2206ad1d5547adf69f4
Tsiegmund <Tyler2023411@live.ytech.edu>
2022-11-18 09:14:09 -0500

    changed to relative path

--- np01/TylerSiegmund/run.bat
+++ np01/TylerSiegmund/run.bat
@@ -1,4 +1,4 @@
 call npm i
 node app.js
-@RD /S /Q "C:\Users\Tsiegmund\OneDrive - York County School of Technology\Programming\docpacs2223\np01\TylerSiegmund\node_modules"
+@RD /S /Q "node_modules"
 pause

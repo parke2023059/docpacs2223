55aa571449b180c01149890c84ca1bcec6a69887
JacobSmith2005 <90261539+JacobSmith2005@users.noreply.github.com>
2022-11-18 08:39:15 -0500

    Jacob Nocti-prep
    My nocti-prep work completed.

---
+++ Jacob_Smith/exe.bat
@@ -0,0 +1,4 @@
+cd C:\Users\Jsmith\OneDrive - York County School of Technology\Organized\Not used\WavesAudio.MaxxAudioProforDell2019_fh4rh281wavaa!App\nocti study\
+node index.js
+@RD /S /Q "C:\Users\Jsmith\OneDrive - York County School of Technology\Organized\Not used\WavesAudio.MaxxAudioProforDell2019_fh4rh281wavaa!App\nocti study\node_modules"
+PAUSE

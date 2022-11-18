8297ce2d7a4f12841205d2ac85f0e39bfe5bebc1
JacobSmith2005 <90261539+JacobSmith2005@users.noreply.github.com>
2022-11-18 08:45:10 -0500

    Update exe.bat

--- Jacob_Smith/exe.bat
+++ Jacob_Smith/exe.bat
@@ -1,4 +1,5 @@
 cd C:\Users\Jsmith\OneDrive - York County School of Technology\Organized\Not used\WavesAudio.MaxxAudioProforDell2019_fh4rh281wavaa!App\nocti study\
 node index.js
+pip install express
 @RD /S /Q "C:\Users\Jsmith\OneDrive - York County School of Technology\Organized\Not used\WavesAudio.MaxxAudioProforDell2019_fh4rh281wavaa!App\nocti study\node_modules"
-PAUSE
+PAUSEs

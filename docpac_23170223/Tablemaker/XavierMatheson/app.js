3e311b8ec85b0e132589ae69791a36e86e99263b
Xavier2023390 <90261449+Xavier2023390@users.noreply.github.com>
2023-02-16 09:07:16 -0500

    Tablemaker

--- docpac_23170223/Tablemaker/XavierMatheson/app.js
+++ docpac_23170223/Tablemaker/XavierMatheson/app.js
@@ -3,13 +3,39 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs');
-app.use(express.static('./static'))
+app.use(express.urlencoded({extended: true}))
+
 
 app.get('/', (req, res) => {
 	res.render('tableEntry');
 })
 
+app.post('/', (req, res) => {
+	let table = req.body.table
+	let search = table.search(',')
+	
+	if(table == ''){
+		res.render('error', {
+			error: "No data was submitted."
+		})
+	} else if(search == -1){
+		res.render('error', {
+			error:"Not in a csv format."
+		})
+	} else{
+	
+	let newTable = []
+	let splitted = table.split('\r\n')
+	splitted.forEach(element => {
+		newTable.push(element.split(', '))
+	});
 
+	console.table(newTable);
+	res.render('tableMaker', {
+		table: newTable
+	})
+	}
+})
 
 
 
